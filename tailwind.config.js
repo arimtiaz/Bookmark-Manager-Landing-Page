22809d4ede97af20e7377fb5f7d30d03684bd86f
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2022-09-01 00:15:28 +0600

    Done With Structuring

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,18 @@ module.exports = {
       xl: "1124px",
       "2xl": "1124px",
     },
-    extend: {},
+    extend: {
+      colors:{
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      },
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
