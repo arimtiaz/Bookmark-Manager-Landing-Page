ff8f53b3cb43b2155cab8717d4cdbcf4cd0cc57b
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2022-09-01 00:07:56 +0600

    Adding

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding: "1rem",
+    },
+    screens:{
+      lg: "1124px",
+      xl: "1124px",
+      "2xl": "1124px",
     },
     extend: {},
   },
