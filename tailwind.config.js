e0f14015c31cf7e54ceb7a4a01b375d3dc944e88
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2022-09-12 15:40:46 +0600

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-  "./public/index.html",
-  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    container: {
-      center: true,
-      padding: "1rem",
-    },
-    screens:{
-      lg: "1124px",
-      xl: "1124px",
-      "2xl": "1124px",
-    },
     extend: {
-      colors:{
+      colors: {
         "bookmark-purple": "#5267DF",
         "bookmark-red": "#FA5959",
         "bookmark-blue": "#242A45",
         "bookmark-grey": "#9194A2",
         "bookmark-white": "#f7f7f7",
       },
-      fontFamily: {
-        Poppins: ["Poppins, sans-serif"],
-      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
